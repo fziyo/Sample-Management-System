e8f68290e445ec7bd1ba6430ad9fa0440c9b74de
fziyo <yangfuzii@outlook.com>
2026-03-13 09:01:04 +0100

    first

---
+++ src/main/java/com/fziyo/sms/SampleManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fziyo.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fziyo/sms/SampleManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fziyo.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
