144e18cc126aa7e4648cba4b514b9d37a5360a7f
fziyo <yangfuzii@outlook.com>
2026-03-26 17:47:10 +0100

    emp controller

--- src/main/java/com/fziyo/sms/common/Result.java
+++ src/main/java/com/fziyo/sms/common/Result.java
@@ -5,23 +5,30 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class Result<T> {
     private Integer code;
     private String msg;
     private T data;
+    private Long timestamp;
+    
+    public Result(Integer code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+        this.timestamp = System.currentTimeMillis();
+    }
     
     public static<T> Result<T> success(){
-        return new Result<>(0, "success", null);
+        return new Result<>(200, "success", null);
     }
     
     public static<T> Result<T> success(T data){
-        return new Result<>(0, "success", data);
+        return new Result<>(200, "success", data);
     }
     
     public static<T> Result<T> error(String msg){
-        return new Result<>(1, msg, null);
+        return new Result<>(500, msg, null);
     }
     
     public static<T> Result<T> error(Integer code, String msg){
--- src/main/java/com/fziyo/sms/controller/EmpController.java
+++ src/main/java/com/fziyo/sms/controller/EmpController.java
@@ -1,14 +1,33 @@
 package com.fziyo.sms.controller;
 
-import com.fziyo.sms.mapper.EmpMapper;
+import com.fziyo.sms.common.Result;
+import com.fziyo.sms.model.dto.EmpCreateDto;
+import com.fziyo.sms.model.vo.EmpVo;
 import com.fziyo.sms.service.EmpService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
+@RequestMapping("/emp")
 @RestController
 public class EmpController {
     @Autowired
     private EmpService empService;
     
+    @PostMapping
+    public Result<Void> add(@RequestBody  EmpCreateDto empCreateDto) {
+        empService.save(empCreateDto);
+        log.info("Saved emp: {}", empCreateDto);
+        return Result.success();
+    }
     
+    @GetMapping
+    public Result<List<EmpVo>> list() {
+        List<EmpVo> empVos = empService.getAll();
+        log.info("Get all employees success, size: {}", empVos.size());
+        return Result.success(empVos);
+    }
 }
